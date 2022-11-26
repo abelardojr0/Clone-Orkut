8cee6cd167e60e56fa8ef485becd2f3f5fcd1193
Abelardo Júnior <abelardo.junior.181@gmail.com>
2022-11-26 15:29:22 -0300

    inicio do projeto

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const menuBotao = document.querySelector("[data-menu-botao]");
+const menuLista = document.querySelector("[data-menu-lista]");
+function clickMenu() {
+  menuLista.classList.toggle("ativo");
+}
+if (menuBotao) {
+  menuBotao.addEventListener("click", clickMenu);
+}
+
+const formLogin = document.querySelector("[data-form-login]");
+
+function logando(event) {
+  event.preventDefault();
+  window.location = "perfil.html";
+}
+formLogin.addEventListener("submit", logando);
